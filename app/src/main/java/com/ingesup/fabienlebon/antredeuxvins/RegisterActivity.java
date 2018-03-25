f9d5ee59bd33d760b38c2b4373507d96277bf093
BlayckPanthers <fabienlebon974@gmail.com>
2018-03-25 13:27:03 +0200

    Ajout res

--- app/src/main/java/com/ingesup/fabienlebon/antredeuxvins/RegisterActivity.java
+++ app/src/main/java/com/ingesup/fabienlebon/antredeuxvins/RegisterActivity.java
@@ -1,27 +1,19 @@
 package com.ingesup.fabienlebon.antredeuxvins;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import com.ingesup.fabienlebon.antredeuxvins.Entities.Wine;
-
-import java.util.Date;
-
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         getSupportActionBar().hide();
-
 
     }
 
-    public void registerOnclick(View view) {
-        Intent i = new Intent();
-
+    public void registerToLoginOnclick(View view) {
     }
 }
